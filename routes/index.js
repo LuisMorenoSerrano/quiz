b45815be345bf2cde502b59f303561a6b49a267e
Luis Moreno <lmoreno.serrano@gmail.com>
2015-07-13 20:48:09 +0200

    Créditos

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,20 @@ var quizController = require('../controllers/quiz_controller');
 // Título del portal
 var sTitulo = 'Quiz';
 
-/* GET home page. */
+// GET / (Página Principal)
 router.get('/', function(req, res, next) {
   res.render('index', { title: sTitulo });
 });
 
+// GET /author (Autor)
+router.get('/author', function(req, res, next) {
+  res.render('author', { title: sTitulo });
+});
+
+// GET /quizes/question (Pregunta)
 router.get('/quizes/question', quizController.question);
+
+// GET /quizes/answer (Respuesta)
 router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
