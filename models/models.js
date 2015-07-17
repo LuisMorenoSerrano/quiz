b733d79a45a57c8df20b9a82d257407bf2966a97
Luis Moreno <lmoreno.serrano@gmail.com>
2015-07-17 20:42:32 +0200

    Despliegue en Heroku (PostgreSQL)

--- models/models.js
+++ models/models.js
@@ -2,10 +2,29 @@
 var path = require('path');
 var Sequelize = require('sequelize');
 
-// Usar BB.DD. SQLite
-var sequelize = new Sequelize(null, null, null,
-  { dialect: 'sqlite', storage: 'quiz.sqlite' }
-  );
+// Definir parámetros de cadena de conexión a la BB.DD.
+//
+// PostgreSQL: DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite....: DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var dialect  = (url[1] || null);
+var protocol = (url[1] || null);
+var user     = (url[2] || null);
+var pwd      = (url[3] || null);
+var host     = (url[4] || null);
+var port     = (url[5] || null);
+var dbname   = (url[6] || null);
+var storage  = process.env.DATABASE_STORAGE;
+
+// Definir cadena de conexión a BB.DD. (SQLite o PostgreSQL)
+var sequelize = new Sequelize(dbname, user, pwd, {
+  host:     host,
+  port:     port,
+  dialect:  dialect,
+  protocol: protocol,
+  storage:  storage,    // Exclusivo de SQLite
+  omitNull: true        // Exclusivo de PostgreSQL
+});
 
 // Importar definición tabla Quiz y exportar
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
